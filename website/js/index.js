dcaebe7435a5395e7a95f91605817f753661a4ad
Ching <dino8556@gmail.com>
2017-08-24 17:36:02 +0800

    add help description

--- website/js/index.js
+++ website/js/index.js
@@ -98,12 +98,73 @@ $('.menu').on('click', function() {
 });
 
 $('body').on('scroll', function(event) {
-  var pos = $(window).scrollTop() + 50;
-
+  var SHOWDIST = $(window).height() * 0.75;
+  var pos = $(window).scrollTop() + SHOWDIST;
   if (isPosBeyondIdTop(pos, '.cloud')) {
     $('.legend-container').removeClass('hidden');
   } else {
     $('.legend-container').addClass('hidden');
+  }
+
+  if (isPosBeyondIdTop(pos, '#word-collection')) {
+    var title = '<h4>詞彙新聞集</h4>'
+    var content = title + '點擊用詞雲中的詞語，可以在詞彙新聞集中查看 6 家媒體報導此詞語的時間軸，與各自的新聞，其中<em>上色者為情緒性報導</em>。'
+    $('#help .content').html(content)
+  } else if (isPosBeyondIdTop(pos, '.cloud')){
+    var title = '<h4>媒體用詞雲</h4>'
+    var content = title + `統計 6 家媒體本週新聞標題內的用詞，在標題內<em>出現次數越多者字體越大</em>。`
+    $('#help .content').html(content)
+  } else if (isPosBeyondIdTop(pos, '.horizontal-lists')) {
+    var title = '<h4>標題用詞報告</h4>'
+    var content = title + `<hr><strong>情緒性報導排行</strong>統計 6 家媒體本週情緒性報導比率的前五名，例如：「台灣  50%」，代表這家媒體所有標題含有「台灣」的報導，標題有 50% 含有情緒性用詞。</p>
+<hr><strong>少量報導主題</strong>分析同一詞語在 6 家媒體中，如果這家媒體對於這個詞語的關注度較低，即為少量報導主題。</p>`
+    $('#help .content').html(content)
+  } else if (isPosBeyondIdTop(pos, '#buzzword')) {
+    var title = '<h4>本週熱詞</h4>'
+    var content = title + `統計本週搜集的所有新聞標題內的用詞，與上週資料做比較，<em>使用次數成長最多者</em>，即為本週的熱詞。
+<hr>
+<strong>情緒性報導</strong>
+透過分析新聞資料的內文與標題，並以詞語向量化 ( word2vec ) 、群聚 ( cluster ) 的技術，找出與「酸」相近的用詞集，並作為以下定義的「情緒性用詞」。
+而情緒性報導是為，新聞標題中含有情緒性用詞的報導，此處統計各家媒體報導本週熱詞時情緒性報導的比率。
+
+「情緒性用詞」包含
+<ul>
+<li>酸</li>
+<li>超狂</li>
+<li>神解答</li>
+<li>氣噗噗</li>
+<li>酸</li>
+<li>超狂</li>
+<li>神解答</li>
+<li>氣噗噗</li>
+<li>酸</li>
+<li>超狂</li>
+<li>神解答</li>
+<li>氣噗噗</li>
+<li>酸</li>
+<li>超狂</li>
+<li>神解答</li>
+<li>氣噗噗</li>
+</ul>
+<hr>
+<strong>報導數量</strong>
+統計 6 家媒體對本周熱詞的報導數量，分析其數量。
+    `
+    $('#help .content').html(content)
+  } else {
+    var title = ''
+    var content = title + `統計6家媒體於官方網站上，一周內所發表之新聞量。
+    <hr><strong>資料來源</strong>
+    <ul>
+    <li>蘋果日報</li>
+    <li>聯合報</li>
+    <li>自由時報</li>
+    <li>東森新聞雲</li>
+    <li>中央通訊社</li>
+    <li>中國時報</li>
+    </ul>
+    `
+    $('#help .content').html(content)
   }
 
 })
