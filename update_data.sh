f0813275bac68998a15164d59ef4e766f891fbb8
Ching <dino8556@gmail.com>
2017-10-09 17:54:15 +0800

    update script

--- update_data.sh
+++ update_data.sh
@@ -9,7 +9,6 @@ cd mediaParser
 scrapy crawl apple -o output/apple_$NOW.json -t json
 scrapy crawl cna -o output/cna_$NOW.json -t json
 scrapy crawl liberty -o output/liberty_$NOW.json -t json
-#scrapy crawl ettoday -o output/ettoday_$NOW.json -t json
 scrapy crawl china -o output/china_$NOW.json -t json
 
 scrapy crawl libertyRealtime -o output/liberty_$NOW.json -t json
--- update_weekly.sh
+++ update_weekly.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
+NOW=$(date +"%Y-%m-%d")
 cd $DIR
-cd word2vec
+cd mediaParser/output
+mv *$NOW.json ../tmp
+cd ../../word2vec
 python3 produceReportByFiles.py ../mediaParser/output/ ../website/prev_report.json
 cd ../mediaParser
 rm -r output/*
+mv tmp/* output
+
