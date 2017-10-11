9562bb03a1245da7a9fce6ed5c29cb53cfec74fc
Ching <dino8556@gmail.com>
2017-10-12 04:19:35 +0800

    add venv in script

--- update_weekly.sh
+++ update_weekly.sh
@@ -2,10 +2,13 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 NOW=$(date +"%Y-%m-%d")
 cd $DIR
+source venv/bin/activate
+
 cd mediaParser/output
 mv *$NOW.json ../tmp
 cd ../../word2vec
 python3 produceReportByFiles.py ../mediaParser/output/ ../website/prev_report.json
+
 cd ../mediaParser
 rm -r output/*
 mv tmp/* output
