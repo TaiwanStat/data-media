6d7c7b54fd715d40946709ac64c29923cdd2b113
Ching <dino8556@gmail.com>
2017-12-05 23:19:03 +0800

    Add cts parser

--- update_daily.sh
+++ update_daily.sh
@@ -3,10 +3,12 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $DIR
 source venv/bin/activate
 NOW=$(date +"%Y-%m-%d")
+YESTERDAY=$(date --date yesterday +"%Y-%m-%d")
 
 cd mediaParser
 scrapy crawl udn -o output/udn_$NOW.json -t json
 scrapy crawl ettoday -o output/ettoday_$NOW.json -t json
+scrapy crawl cts -o output/cts_$YESTERDAY.json -t json
 cd ../
 python3 merge_daily.py
 python3 verify.py | grep $NOW
@@ -18,4 +20,5 @@ aws s3 cp output/udn_$NOW.json s3://tw-media-data/
 aws s3 cp output/liberty_$NOW.json s3://tw-media-data/
 aws s3 cp output/ettoday_$NOW.json s3://tw-media-data/
 aws s3 cp output/china_$NOW.json s3://tw-media-data/
+aws s3 cp output/cts_$YESTERDAY.json s3://tw-media-data/
 
