b06f46c9445ef2e88dafe871cbde89f24c58ce67
Ching <dino8556@gmail.com>
2018-01-31 11:41:32 +0800

    Add command of execute setn crawler

--- update_data.sh
+++ update_data.sh
@@ -13,6 +13,7 @@ scrapy crawl china -o output/china_$NOW.json -t json
 
 scrapy crawl libertyRealtime -o output/liberty_$NOW.json -t json
 scrapy crawl appleRealtime -o output/apple_$NOW.json -t json
+scrapy crawl setn -o output/setn_$NOW.json -t json -s  DOWNLOAD_DELAY=1.5
 
 cd ../
 echo $(date +"%Y-%m-%d %H")
