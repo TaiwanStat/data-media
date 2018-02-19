a07dc20a1c84be8b0a73e9ef04c5723d8fe60ef5
Ching <dino8556@gmail.com>
2018-02-20 02:19:13 +0800

    Fix setn date naming

--- update_data.sh
+++ update_data.sh
@@ -4,13 +4,14 @@ cd $DIR
 source venv/bin/activate
 
 NOW=$(date +"%Y-%m-%d")
+YESTERDAY=$(date --date yesterday +"%Y-%m-%d")
 cd mediaParser
 
 scrapy crawl apple -o output/apple_$NOW.json -t json
 scrapy crawl cna -o output/cna_$NOW.json -t json
 scrapy crawl liberty -o output/liberty_$NOW.json -t json
 scrapy crawl china -o output/china_$NOW.json -t json
-scrapy crawl setn -o output/setn_$NOW.json -t json -s  DOWNLOAD_DELAY=1
+scrapy crawl setn -o output/setn_$YESTERDAY.json -t json -s  DOWNLOAD_DELAY=1
 scrapy crawl pts -o output/pts_$NOW.json -t json
 scrapy crawl tvbs -o output/tvbs_$NOW.json -t json
 
